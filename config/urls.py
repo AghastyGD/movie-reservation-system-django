9b207307de86cfb74f7f9172104e76cc2d9faaef
AghastyGD <augustodomingosvasco@gmail.com>
2024-08-31 13:02:38 +0200

    create Movie model

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path('admin/', admin.site.urls),
     path("users/", include("users.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    poster_image = models.ImageField(upload_to="movie_poster/")
+    genre = models.ManyToManyField(Genre)
+    
+    def __str__(self):
+        return self.title
+
