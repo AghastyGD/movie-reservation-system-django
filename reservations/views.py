ff50408c9fe16f67b1c0878711fbe2b10e8ae789
AghastyGD <augustodomingosvasco@gmail.com>
2024-09-02 12:09:07 +0200

    create movie list, detail and reservation_form templates

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Movie, Showtime, Seat, Reservation
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    
+    context = {
+        'movies': movies
+    }
+    return render(request, "reservations/movie_list.html", context)
+
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    showtimes = Showtime.objects.filter(movie=movie)
+    
+    context = {
+        'movie': movie,
+        'showtimes': showtimes
+    }
+    return render(request, 'reservations/movie_detail.html', context)
+
+
+def reservation_form(request, showtime_id):
+    showtime = get_object_or_404(Showtime, pk=showtime_id)
+    seats = Seat.objects.filter(showtime=showtime, is_available=True)
+    
+    if request.method == 'POST':
+        seat_id = request.POST.get('seat')
+        seat = get_object_or_404(Seat, pk=seat_id)
+        reservation = Reservation(user=request.user, seat=seat)
+        reservation.save()
+        return redirect('movie_list')
+    
+    context = {
+        'showtime': showtime,
+        'seats': seats
+    }
+    
+    return render(request, 'reservations/reservation_form.html', context)
