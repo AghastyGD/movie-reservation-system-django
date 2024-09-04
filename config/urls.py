892a5b44109cd507006cfaa9eb58e23fab785c89
Augusto Domingos <augustodomingosvasco@gmail.com>
2024-09-04 14:27:03 +0200

    add css

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
+from reservations.views import home 
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", home, name="home"),  
     path('admin/', admin.site.urls),
     path("users/", include("users.urls")),
     path("movies/", include("reservations.urls")),
