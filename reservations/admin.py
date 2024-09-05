fa36561a9c0e4f6133d67855ec76e78fa704d0f5
Augusto Domingos <augustodomingosvasco@gmail.com>
2024-09-05 14:31:09 +0200

    add email field in signup form and update admin to show reservations reports

--- reservations/admin.py
+++ reservations/admin.py
@@ -9,11 +9,15 @@ class GenreAdmin(admin.ModelAdmin):
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
+    list_filter = ('genre',)
+    search_fields = ('title',)
 
 @admin.register(Showtime)
 class ShowtimeAdmin(admin.ModelAdmin):
     list_display = ('movie', 'formatted_start_time', 'formatted_end_time')
-
+    list_filter = ('movie', 'start_time')
+    search_fields = ('movie__title',)
+    
     def formatted_start_time(self, obj):
         return obj.formatted_start_time
     formatted_start_time.short_description = 'Start Time'
@@ -21,17 +25,41 @@ class ShowtimeAdmin(admin.ModelAdmin):
     def formatted_end_time(self, obj):
         return obj.formatted_end_time
     formatted_end_time.short_description = 'End Time'
-    
 
 @admin.register(Seat)
 class SeatAdmin(admin.ModelAdmin):
     list_display = ('showtime', 'seat_number', 'is_available')
-    
+    list_filter = ('showtime', 'is_available')
+    search_fields = ('showtime__movie__title', 'seat_number')
+
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ('user', 'seat', 'formatted_reservation_time')
-    
+    list_filter = ('seat__showtime__movie', 'seat__showtime__start_time')
+    search_fields = ('user__username', 'seat__showtime__movie__title')
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=extra_context)
+        
+        try:
+            qs = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+
+        summary = self.get_summary_data(qs)
+        response.context_data['summary'] = summary
+
+        return response
+
+    def get_summary_data(self, queryset):
+        total_reservations = queryset.count()
+        total_capacity = Seat.objects.count()
+
+        return {
+            'total_reservations': total_reservations,
+            'total_capacity': total_capacity,
+        }
+
     def formatted_reservation_time(self, obj):
         return obj.formatted_reservation_time
     formatted_reservation_time.short_description = 'Reservation Time'
-    
--- users/forms.py
+++ users/forms.py
@@ -3,18 +3,20 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    
     usable_password = None
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].label = 'Username'
+        self.fields['email'].label = 'Email'
         self.fields['username'].help_text = None
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
         
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
         
     
 class CustomUserChangeForm(UserChangeForm):
